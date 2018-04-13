156d7c471e5f30da8c6726765e90747c55b75561
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2018-04-12 20:39:25 -0400

    tv api implemented

--- public/js/members.js
+++ public/js/members.js
@@ -3,5 +3,25 @@ $(document).ready(function() {
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
+    console.log("user data function");
   });
+
+
+
+  // This function handles events where a movie button is clicked
+  $(".searchTV").on("click", function(event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var show = $(".tv-input").val().trim();
+    var queryURL = "http://api.tvmaze.com/search/shows?q=" + show;
+
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+    });
+  });
+
 });
