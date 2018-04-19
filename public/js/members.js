5cff48043c04fed0f44298a658af0da0380a720f
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2018-04-18 22:38:48 -0400

    organize code into functions

--- public/js/members.js
+++ public/js/members.js
@@ -1,21 +1,37 @@
 
 
 $(document).ready(function() {
-  getDay();
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.name);
-  });
 
-  // This function handles events where a movie button is clicked
-  $(".searchTV").on("click", function(event) {
-    event.preventDefault();
+    // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    $.get("/api/user_data").then(function(data) {
+      $(".member-name").text(data.name);
+    });
 
+    // Display today's day
+    getDay();
+
+    // Display show info on search button click
+    $(".searchTV").on("click", function(event) {
+      searchBar();
+    });
+
+});  // End document ready
+
+// ===============
+// FUNCTIONS
+// ===============
+
+function searchBar() {
+  event.preventDefault();
+
+    // clear show info 
     $("#episode-results").empty();
+
     // This line grabs the input from the textbox
     var show = $(".tv-input").val().trim();
 
+    // API get show info query
     var showInfoQuery = "http://api.tvmaze.com/search/shows?q=" + show + "&embed=episodes";
 
     $.ajax({
@@ -32,85 +48,94 @@ $(document).ready(function() {
       $("#results").html("<p>" + response[0].show.schedule.days + " " + response[0].show.schedule.time + "</p>");
       $("#premiere").html("<p><strong>Premiere Date: </strong>" + response[0].show.premiered + "</p>");
       $("#results").html("<p>" + response[0].show.status + "</p>");
-      
+  
+      // if the show has an external site display link
       if(response[0].show.officialSite !== null) {
         $("#site-link").html("<a target='_blank' href='" + response[0].show.officialSite + "'>" + response[0].show.officialSite + "</a>");
+      // else display nothing
       } else {
-        $("site-link").css("display:none");
+        $("#site-link").empty();
       }
 
+      // If show is on air display "Add to watchlist" button
       if (response[0].show.status == "Running") {
         $(".watchlist-btn").html("<a class='btn btn-outline-warning add-to-watchlist-btn'>Add to Watchlist</a>");
+      // else display nothing
       } else {
-
+        $(".watchlist-btn").empty();
       }
 
 
-      var showID = response[0].show.id;
-      var seasonsQuery = "http://api.tvmaze.com/shows/" + showID + "/seasons";
-        $.ajax({
-          url: seasonsQuery,
-          method: "GET"
-        }).done(function(data) {
-          console.log(data);
-          for (var i = 0; i < data.length; i++) {
-            var seasonID = data[i].id;
-            console.log(seasonID);
-            $("#season-btns").append("<button class='btn btn-warning season-btn' id='" + i + "' data-id='" + seasonID + "'>Season: " + data[i].number + "</button>");
 
-            var seasonBtn = $("#" + i);
+      getSeasons();
 
-            seasonBtn.click(function() {
-              var seasonBtnID = $(this).attr("data-id");
-              var episodesQuery = "http://api.tvmaze.com/seasons/" + seasonBtnID + "/episodes"; //number must be seasonID
-              $.ajax({
-                url: episodesQuery,
-                method: "GET"
-              }).done(function(episodes) {
-                console.log(episodes);
-                for (var i = 0; i < episodes.length; i++) {
-                  $("#episodes").append("<p>" + episodes[i].name + "</p>");
-                }
-                
+        function getSeasons() {
+          var showID = response[0].show.id;
+          var seasonsQuery = "http://api.tvmaze.com/shows/" + showID + "/seasons";
+
+          $.ajax({
+            url: seasonsQuery,
+            method: "GET"
+          }).done(function(seasons) {
+            console.log(seasons);
+
+            for (var i = 0; i < seasons.length; i++) {
+              var seasonID = seasons[i].id;
+              console.log(seasonID);
+              $("#season-btns").append("<button class='btn btn-warning season-btn' id='" + i + "' data-id='" + seasonID + "'>Season: " + seasons[i].number + "</button>");
+
+              var seasonBtn = $("#" + i);
+
+              seasonBtn.click(function() {
+                var seasonBtnID = $(this).attr("data-id");
+                var episodesQuery = "http://api.tvmaze.com/seasons/" + seasonBtnID + "/episodes"; //number must be seasonID
+
+                $.ajax({
+                  url: episodesQuery,
+                  method: "GET"
+                }).done(function(episodes) {
+                  console.log(episodes);
+                  
+                  for (var i = 0; i < episodes.length; i++) {
+                    $("#episodes").append("<p>" + episodes[i].name + "</p>");
+                  }
+                });
+
               });
-              
-            });
-          }
-        });
-    });
+            } // End for loop
+          }); // End seasonsQuery Ajax call
+        } // End getSeasons function
 
-  });
+    }); // End getShow Ajax call
+} // End searchBar function
 
-  function getDay() {
-    let day;
-    switch (new Date().getDay()) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-    }
-  
-    console.log(day);
-    $("#time").text(day);
+// Get the current day and display on the page
+function getDay() {
+  let day;
+  switch (new Date().getDay()) {
+      case 0:
+          day = "Sunday";
+          break;
+      case 1:
+          day = "Monday";
+          break;
+      case 2:
+          day = "Tuesday";
+          break;
+      case 3:
+          day = "Wednesday";
+          break;
+      case 4:
+          day = "Thursday";
+          break;
+      case 5:
+          day = "Friday";
+          break;
+      case 6:
+          day = "Saturday";
+          break;
+      default:
   }
-  
-
-});
+  console.log(day);
+  $("#time").html("<p>Today is " + day + "</p>");
+};
